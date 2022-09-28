e28122c0a2a2af038970088fbea795bf1e8b7ba7
Yogesh saini <101331023+yogeshsaini21@users.noreply.github.com>
2022-09-28 21:24:44 +0530

    BankAccountService complete

--- Payment-Wallet-Application/src/main/java/com/masai/ServiceLayer/BankAccountService.java
+++ Payment-Wallet-Application/src/main/java/com/masai/ServiceLayer/BankAccountService.java
@@ -1,5 +1,18 @@
 package com.masai.ServiceLayer;
 
+import java.util.List;
+
+import com.masai.Models.BankAccount;
+import com.masai.Models.Wallet;
+
 public interface BankAccountService {
 
+	public Wallet addAccount(BankAccount bacc);
+	
+	public Wallet removeAccount(BankAccount bacc);
+	
+	
+	public BankAccount viewAccount(Wallet wallet);
+
+	public List<BankAccount> viewAllAccount(Wallet wallet);
 }
