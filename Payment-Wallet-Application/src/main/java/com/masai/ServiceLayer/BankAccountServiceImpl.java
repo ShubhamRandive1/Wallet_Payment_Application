5636b47ba48e1c52192c2347f03b6f2bc635b9ae
Yogesh saini <101331023+yogeshsaini21@users.noreply.github.com>
2022-09-28 21:27:12 +0530

    BankAccountServiceImpl

--- Payment-Wallet-Application/src/main/java/com/masai/ServiceLayer/BankAccountServiceImpl.java
+++ Payment-Wallet-Application/src/main/java/com/masai/ServiceLayer/BankAccountServiceImpl.java
@@ -1,5 +1,58 @@
 package com.masai.ServiceLayer;
 
-public class BankAccountServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.masai.Models.BankAccount;
+import com.masai.Models.Wallet;
+import com.masai.Repository.BankAccountDao;
+
+public class BankAccountServiceImpl implements  BankAccountService {
+
+	@Autowired
+	private BankAccountDao BADAO;
+
+	@Override
+	public Wallet addAccount(BankAccount bacc) {
+		
+		BankAccount bankAccount = BADAO.save(bacc);
+  
+		Wallet wallet =	bankAccount.getWallet();
+		return wallet;
+	}
+
+	@Override
+	public Wallet removeAccount(BankAccount bacc) {
+		Optional<BankAccount> bankAccount  = BADAO.findById(bacc.getAccountNo());
+		
+		if(bankAccount.get() != null) {
+			BADAO.delete(bacc);
+			
+			return bankAccount.get().getWallet();
+		}
+		else {
+			throw new BankAccountException("Bank Account with this a/n does't exist : "+bacc.getAccountNo());
+		}
+		
+		
+		
+
+	}
+
+	@Override
+	public BankAccount viewAccount(Wallet wallet) {
+		return null;
+	}
+
+	@Override
+	public List<BankAccount> viewAllAccount(Wallet wallet) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+	
 }
