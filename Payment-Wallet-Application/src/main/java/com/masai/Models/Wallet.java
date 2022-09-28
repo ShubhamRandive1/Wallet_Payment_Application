cf61a05e787f8d4b1ae9cc5770fb584cc900e232
Shubham C Randive <57911117+ShubhamRandive1@users.noreply.github.com>
2022-09-28 08:00:58 +0530

    wallet not all method
    only few method created in order to implement login session

--- Payment-Wallet-Application/src/main/java/com/masai/Models/Wallet.java
+++ Payment-Wallet-Application/src/main/java/com/masai/Models/Wallet.java
@@ -1,9 +1,33 @@
 package com.masai.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Getter
+@Setter
 public class Wallet {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer walletId;
+	
+	@NotNull
+	@Min(value = 0, message = "Balance Should Not Be In Negative")
+	private double balance;
 	
 
 }
+
