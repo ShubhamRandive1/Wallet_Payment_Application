2ca35034e6fe409fa714f773260957dca5df569c
Shubham C Randive <57911117+ShubhamRandive1@users.noreply.github.com>
2022-09-28 07:59:46 +0530

    login session created

--- Payment-Wallet-Application/src/main/java/com/masai/Models/loginSession.java
+++ Payment-Wallet-Application/src/main/java/com/masai/Models/loginSession.java
@@ -1,5 +1,42 @@
- package com.masai.Models;
+package com.masai.Models;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class loginSession {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer loginSessionId;
+	
+	
+	@Column(unique = true)
+	private Integer customerId;
+	
+	private String loginUserId;
+	
+	private LocalDateTime localDateTime;
+
+	public loginSession(Integer customerId, String loginUserId, LocalDateTime localDateTime) {
+		super();
+		this.customerId = customerId;
+		this.loginUserId = loginUserId;
+		this.localDateTime = localDateTime;
+	}
+	
+	
+	
 
 }
