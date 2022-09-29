c843093452af74e5a741134c0495fc6014989df1
Shubham C Randive <57911117+ShubhamRandive1@users.noreply.github.com>
2022-09-29 13:04:37 +0530

    Add files via upload
    DTos for bankAcc and customer

---
+++ Payment-Wallet-Application/src/main/java/com/masai/DTO/BankAccountDTO.java
@@ -0,0 +1,20 @@
+package com.masai.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankAccountDTO {
+	
+	private Integer accountNo;
+	
+	private String ifscCode;
+	
+	private String bankName;
+	
+	private double balance;
+
+}
