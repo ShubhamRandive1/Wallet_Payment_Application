504b2dbd800b4cc0d90edf139a1236fa9809133e
Yogesh saini <101331023+yogeshsaini21@users.noreply.github.com>
2022-09-29 15:33:32 +0530

    Update BankAccount.java

--- Payment-Wallet-Application/src/main/java/com/masai/Models/BankAccount.java
+++ Payment-Wallet-Application/src/main/java/com/masai/Models/BankAccount.java
@@ -1,5 +1,52 @@
 package com.masai.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class BankAccount {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer accountNo;
+	
+	@NotNull
+	@Pattern(regexp = "^[A-Z]{4}0[A-Z0-9]{6}")
+	private String ifscCode;
+	
+	@NotNull
+	@Size(min = 7, max = 20, message = "Bank Name must have min 7 and max 15 characters")
+	private String bankname;
+	
+	@NotNull
+	@Min(0)
+	private Integer balance;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JsonIgnore
+	private  Wallet wallet;
+	
+	
+	
+	
 }
