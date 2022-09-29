95641334e84b80195e2c7cd7a45d14dc1c841cf5
Yogesh saini <101331023+yogeshsaini21@users.noreply.github.com>
2022-09-29 11:17:58 +0530

    Update BankAccount.java

--- Payment-Wallet-Application/src/main/java/com/masai/Models/BankAccount.java
+++ Payment-Wallet-Application/src/main/java/com/masai/Models/BankAccount.java
@@ -1,5 +1,32 @@
 package com.masai.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class BankAccount {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer accountNo;
+	private String ifscCode;
+	private String bankname;
+	private Integer balance;
+	private  Wallet wallet;
+	
+	
+	
+	
 }
