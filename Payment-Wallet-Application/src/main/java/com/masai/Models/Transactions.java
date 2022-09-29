bb3b019b03bafb817cb69c99d878610115037dd7
Shubham C Randive <57911117+ShubhamRandive1@users.noreply.github.com>
2022-09-29 12:43:35 +0530

    Update Transactions.java
    transaction done

--- Payment-Wallet-Application/src/main/java/com/masai/Models/Transactions.java
+++ Payment-Wallet-Application/src/main/java/com/masai/Models/Transactions.java
@@ -1,5 +1,43 @@
- package com.masai.Models;
+package com.masai.Models;
 
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Transactions {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer transactionId;
+	private String transactionType;
+	
+	@CreatedDate
+	@CreationTimestamp
+	@JsonFormat(pattern = "dd-MM-yyyy")
+	private LocalDate transactionDate;
+	private double amount;
+	private String description;
+	
+	@ManyToOne
+	@JsonIgnore
+	private Wallet wallet;
+	
 
 }
