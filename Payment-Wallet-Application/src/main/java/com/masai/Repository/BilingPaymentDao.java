de9237466894d482feab19e671d98eec2a31eac8
Bhanuprathap Goud <101325107+BhanuPrathapGoud@users.noreply.github.com>
2022-09-29 12:31:41 +0530

    BillingPaymentDao added

--- Payment-Wallet-Application/src/main/java/com/masai/Repository/BilingPaymentDao.java
+++ Payment-Wallet-Application/src/main/java/com/masai/Repository/BilingPaymentDao.java
@@ -1,5 +1,11 @@
 package com.masai.Repository;
 
-public interface BilingPaymentDao {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.masai.Models.BillingPayment;
+
+@Repository
+public interface BillingPaymentDao extends JpaRepository<BillingPayment, Integer> {
 
 }
